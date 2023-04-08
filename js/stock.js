d5b229607fc6127209b21c2d58561346972a4736
Andres Amuchastegui <andres.martin.amuchastegui@gmail.com>
2023-04-08 13:31:15 -0300

    tercera

---
+++ js/stock.js
@@ -0,0 +1,18 @@
+let stockProductos = [
+    { id: 1, nombre: "CAZUELA APILABLE", tipo: "contenerdor", cantidad: 1, precio: 1400, img: './img/1.jpg' },
+    { id: 2, nombre: "HERMÉTICO RECTANGULAR", tipo: "contenerdor", cantidad: 1, precio: 450, img: './img/2.jpg' },
+    { id: 3, nombre: "BATIDOR VIOLETA", tipo: "utensilio", cantidad: 1, precio: 800, img: './img/3.jpg' },
+    { id: 4, nombre: "RALLADOR DE AJO VERDE", tipo: "utensilio", cantidad: 1, precio: 400, img: './img/4.jpg' },
+    { id: 5, nombre: "FRASCO TAPA DE ACERO", tipo: "contenerdor", cantidad: 1, precio: 1600, img: './img/5.jpg' },
+    { id: 6, nombre: "PELAPAPA CLÁSICO", tipo: "utensilio", cantidad: 1, precio: 600, img: './img/6.jpg' },
+    { id: 7, nombre: "CEPILLO LIMPIA BOMBILLAS", tipo: "utensilio", cantidad: 1, precio: 300, img: './img/7.jpg' },
+    { id: 8, nombre: "BOWL LISO CELESTE", tipo: "contenerdor", cantidad: 1, precio: 1600, img: './img/8.jpg' },
+    { id: 9, nombre: "BOWL LISO NEGRO", tipo: "contenerdor", cantidad: 1, precio: 1600, img: './img/9.jpg' },
+    { id: 10, nombre: "FRASCO AMANECER", tipo: "contenerdor", cantidad: 1, precio: 250, img: './img/10.jpg' },
+    { id: 11, nombre: "TOSTADOR SIMPLE MOTEADO", tipo: "utensilio", cantidad: 1, precio: 1200, img: './img/11.jpg' },
+    { id: 12, nombre: "BUDINERA", tipo: "utensilio", cantidad: 1, precio: 1350, img: './img/12.jpg' },
+    { id: 13, nombre: "PISA PAPAS", tipo: "utensilio", cantidad: 1, precio: 990, img: './img/13.jpg' },
+    { id: 14, nombre: "ENSALADERA MADERA", tipo: "contenerdor", cantidad: 1, precio: 2400, img: './img/14.jpg' },
+    { id: 15, nombre: "MANGA REPOSTERA", tipo: "utensilio", cantidad: 1, precio: 800, img: './img/15.jpg' },
+    { id: 16, nombre: "SALERO DE MESA", tipo: "utensilio", cantidad: 1, precio: 350, img: './img/16.jpg' },
+]
